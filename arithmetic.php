fd87d05e252e946470e638bd6982da6a121203f0
megaminded <megamindsetsolutions@gmail.com>
2021-03-12 11:58:36 +0100

    Learnt the use the arithmetic operators in PHP

---
+++ arithmetic.php
@@ -0,0 +1,41 @@
+<?php
+// Arithmetic operators
+$cp = 100;
+$sp = 150;
+$profit = $sp - $cp; // Subtraction
+// echo $profit;
+
+
+$students = 10;
+$staffs = 5;
+$human_being = $students + $staffs; // Addition
+// echo $human_being;
+
+
+$orange_price = 200;
+$quantity = 5;
+$total = $orange_price * $quantity; // Multiplication
+// echo $total;
+
+
+$orange_total_price = $total;
+$per_price = $orange_total_price / $quantity; // Division
+// echo $per_price;
+
+
+$a_number = 10;
+$divisible_by_3 = $a_number % 2; // Modulus
+// echo $divisible_by_3;
+
+
+$base_value = 10;
+$exponential = $base_value ** 2;
+
+// echo $exponential;
+
+// Brackets of division, multiplication, addition and subtraction; BODMAS
+// where x = 2;
+// 5x+1/2-4x;
+$x = 2;
+// echo 5 * $x + 1 / 2 - 4 * $x;
+// echo ((5 * $x) + (1 / 2)) - (4 * $x);
---
+++ strings.php
@@ -0,0 +1,10 @@
+<?php
+$first_name = "John ";
+$last_name = "Emeke";
+
+$first_name .= " K ";
+
+
+$fullname = $first_name . $last_name;
+
+echo $fullname;
