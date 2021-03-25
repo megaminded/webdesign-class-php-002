b938a856c6b02402c77cf87168ca29500cdab57f
megaminded <megamindsetsolutions@gmail.com>
2021-03-25 11:15:18 +0100

    Learnt the basic use of inbuilt functions and constants

---
+++ fundementals/constants.php
@@ -0,0 +1,17 @@
+<?php
+// Variables can be altered
+// $variableName = "This is a boy";
+
+// echo $variableName;
+
+// $variableName = "This is a girl";
+
+// echo $variableName;
+
+// Constants cannot be altered
+define('SITE_NAME', 'Google.com');
+
+echo SITE_NAME;
+
+// This will not work
+define('SITE_NAME', 'Something else');
---
+++ fundementals/inbuilt_functions.php
@@ -0,0 +1,27 @@
+<?php
+$users = ['Emeke', 'Peters'];
+// They are logics built into the language to help us carry a specific operation
+# Help us count every elements in the array
+// echo count($users);
+
+// $items = 0;
+// foreach ($users as $key => $value) {
+//     $items++;
+// }
+// echo $items;
+
+# Help us add elements in the end of an array 
+// array_push($users, 'James');
+
+$sentence  = 'This is a welcome message';
+# Helps us get the length of a string
+$length = strlen($sentence);
+// echo $length;
+
+$uppercase = strtoupper($sentence);
+
+// echo $uppercase;
+
+$lowercase = strtolower($sentence);
+
+echo $lowercase;
